c2e686ab4507e81271f1364331e26c47d19b2d24
Ruiz-Martinez-Andriks-Imanol <105221206+Ruiz-Martinez-Andriks-Imanol@users.noreply.github.com>
2022-05-14 17:57:18 -0500

    prog_est_prog15
    Un programa que da N números, el programa debe preguntar cuantos números quieres y después pide cada número

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # prog_est_prog15
-Un programa que da N números, el programa debe preguntar cuantos números quieres y después pide cada número
+Autor : Andriks Imanol Ruiz Martínez, Fecha: 21/02/2022 <br>
+Escuela: Universidad del Valle de Mexico Campus Villahermosa <br>
+Materia: Programación Estructrada <br>
+Ciclo: 01/2022</p>
+<br>
+<p>Este es un programa de ciclos en Lenguaje C de la materia de Programación Estructurada<br>
+Muestra el uso de:
+  <ul>
+    <li>Variables enteras y flotantes</li>
+    <li>printf para mostrar varias variables</li>
+    <li>scanf</li>
+    <li>El uso de include para las librerias</li>
+    <li>for</li>
+    <li>Contador en el for</li>
+    <li>Comentarios para la doumentación interna del programa.</li>
+    </ul>
+    </p>
+<br>
+Objetivo:el programa debe preguntar cuantos números quieres y después pide cada número.
+<br>
+<br>
+<p>Breve descripción:
+El programa aquí pide que indiquemos cuantos numeros deseamos que lea para realizar un ciclo con la cantidad de veces que pedimos, al igual que suma todos los números que pusimos.
+<br>
+</p>
---
+++ prog15.cpp
@@ -0,0 +1,40 @@
+/* Autor: Andriks Imanol Ruiz Mártinez, Realizado: 21/02/2022
+Escuela: Universidad del Valle de Mexico Campus Villahermosa 
+Materia: Programación Estructrada
+Ciclo: 01/2022
+
+Este es un programa de ciclos en Lenguaje C de la materia de Programación Estructurada
+Muestra el uso de:
+
+    -Variables enteras y flotantes
+    -printf para mostrar varias variables
+    -scanf
+    -El uso de include para las librerias
+    -for
+    -Contador en el for
+    -Comentarios para la documentación interna del programa.
+*/
+#include<stdio.h>
+int main() {
+	//Declaración de variables
+	float numero, suma=0;
+	int cuantos;
+	//Procesos
+	printf("Cuantos numeros deseas leer: ");
+	scanf("%d", &cuantos);
+	//for(int i=1;i<=20;i=i+2)
+	//for(int i=1;i<=20;i+=2)
+	//for(int i=1;i<=20;i++)
+	//for(int i=20;i>=1;i-=3)
+//	for(int i=20;i>=1;i--)
+	for(int i=1;i<=cuantos;i++){ //Ciclo for
+	//	printf("Introduce un numero %d: ", i);
+		printf("Introduce el valor numero %d: ", i);
+		scanf("%f", &numero);
+		//suma=suma+numero;
+		suma+=numero;
+	}
+	//Salida
+	printf("La suma es %.2f", suma);
+	return 0;	
+}
